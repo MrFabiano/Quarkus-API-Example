6453694a4c77d29e62742c68eeb6258102dbaa1d
fabiano <fabiannosanttos@gmail.com>
2024-05-15 16:23:29 -0300

    create conection base postgres sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,20 @@
-insert into Fruta(id, nome, qtd) values (1, 'Maça', 3);
-insert into Fruta(id, nome, qtd) values (2, 'Pera', 5);
-insert into Fruta(id, nome, qtd) values (3, 'Uva', 4);
+DROP TABLE IF EXISTS Fruta;
+DROP SEQUENCE IF EXISTS fruta_seq;
+
+CREATE SEQUENCE fruta_seq START 1;
+
+CREATE TABLE Fruta (
+    id BIGINT NOT NULL DEFAULT nextval('fruta_seq'),
+    nome VARCHAR(255),
+    qtd INTEGER,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO Fruta (id, nome, qtd) VALUES (nextval('fruta_seq'), 'Maca', 3);
+INSERT INTO Fruta (id, nome, qtd) VALUES (nextval('fruta_seq'), 'Pera', 5);
+INSERT INTO Fruta (id, nome, qtd) VALUES (nextval('fruta_seq'), 'Uva', 4);
+
+--CREATE SEQUENCE hibernate_sequence;
+--insert into Fruta(id, nome, qtd) values (nextval('hibernate_sequence'), 'Maca', 3);
+--insert into Fruta(id, nome, qtd) values (nextval('hibernate_sequence'), 'Pera', 5);
+--insert into Fruta(id, nome, qtd) values (nextval('hibernate_sequence'), 'Uva', 4);
