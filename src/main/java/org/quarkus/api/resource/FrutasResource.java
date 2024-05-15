397204cedf54b6e142e0c2e920b6bb668a0fb8ef
fabiano <fabiannosanttos@gmail.com>
2024-05-15 16:24:54 -0300

    create method getById, update, delete, ad crudo quarkus

--- src/main/java/org/quarkus/api/resource/FrutasResource.java
+++ src/main/java/org/quarkus/api/resource/FrutasResource.java
@@ -1,12 +1,11 @@
 package org.quarkus.api.resource;
 
 import jakarta.inject.Inject;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.quarkus.api.dto.FrutaDTO;
 import org.quarkus.api.model.Fruta;
 import org.quarkus.api.service.FrutaService;
@@ -27,8 +26,43 @@ public class FrutasResource {
     }
 
     @POST
-    public void novaFruta(FrutaDTO  frutaDTO){
-        frutaService.novaFruta(frutaDTO);
+    public Response novaFruta(FrutaDTO  frutaDTO){
+        Fruta fruta = frutaService.novaFruta(frutaDTO);
+        return Response.status(Response.Status.CREATED).entity(fruta).build();
+    }
 
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public Response updatePorId(@PathParam("id")Long id, FrutaDTO frutaDTO){
+        Fruta frutaExiste = frutaService.buscarPorId(id);
+        if (frutaExiste == null){
+            throw new EntityNotFoundException("Fruta com ID " + id + "Não encontrada");
+        }
+
+        frutaExiste.setNome(frutaDTO.getNome());
+        frutaExiste.setQtd(frutaDTO.getQtd());
+
+        Fruta frutaAtualizada = frutaService.update(frutaExiste);
+        return Response.ok(frutaAtualizada).build();
+    }
+
+    @GET()
+    @Path("/{id}")
+    public Fruta getFrutaId(@PathParam("id") Long id){
+        return frutaService.buscarPorId(id);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response deleteById(@PathParam("id") Long id){
+        Fruta fruta = frutaService.buscarPorId(id);
+        if(fruta == null){
+            throw new EntityNotFoundException("Fruta com ID" + id + "Não encontrada");
+        }
+
+        frutaService.deletar(fruta);
+        return Response.status(Response.Status.NO_CONTENT).build();
     }
 }
--- src/main/java/org/quarkus/api/service/FrutaService.java
+++ src/main/java/org/quarkus/api/service/FrutaService.java
@@ -2,6 +2,7 @@ package org.quarkus.api.service;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.quarkus.api.dto.FrutaDTO;
 import org.quarkus.api.model.Fruta;
@@ -10,10 +11,13 @@ import org.quarkus.api.model.IndentificadorTransacao;
 import java.util.List;
 
 @ApplicationScoped
-public class FrutaService {
+public final class FrutaService {
 
     @Inject
     IndentificadorTransacao indentificadorTransacao;
+
+    @Inject
+    EntityManager entityManager;
 
     public List<Fruta> list(){
         System.out.println(indentificadorTransacao.getIndentificadorTransacao());
@@ -21,12 +25,28 @@ public class FrutaService {
     }
 
     @Transactional
-    public void novaFruta(FrutaDTO  frutaDTO){
+    public Fruta novaFruta(FrutaDTO  frutaDTO){
         System.out.println(indentificadorTransacao.getIndentificadorTransacao());
         Fruta fruta = new Fruta();
         fruta.setNome(frutaDTO.getNome());
         fruta.setQtd(frutaDTO.getQtd());
         fruta.persist();
 
+        return fruta;
+
+    }
+
+    @Transactional
+    public Fruta update(Fruta fruta){
+        return entityManager.merge(fruta);
+    }
+
+    public Fruta buscarPorId(Long id){
+        return entityManager.find(Fruta.class, id);
+    }
+
+    @Transactional
+    public void deletar(Fruta fruta){
+      entityManager.remove(fruta);
     }
 }
